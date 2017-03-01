01f36d94ae01d4f0c1bb5f8ea3d77730907e45eb
Olle Törnström <olle@studiomediatech.com>
2017-03-01 23:41:18 +0100

    Initial commit

---
+++ src/main/java/com/studiomediatech/AssetsStoreApplication.java
@@ -0,0 +1,12 @@
+package com.studiomediatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetsStoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/studiomediatech/AssetsStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.studiomediatech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AssetsStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
