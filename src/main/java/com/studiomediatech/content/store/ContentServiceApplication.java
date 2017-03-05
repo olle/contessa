5e6198637f13dc3ee5416ce5d5f9129a0b38923d
Olle Törnström <olle@studiomediatech.com>
2017-03-05 11:06:38 +0100

    Moved application definition

--- src/main/java/com/studiomediatech/content/store/ContentServiceApplication.java
+++ src/main/java/com/studiomediatech/content/store/ContentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.studiomediatech;
+package com.studiomediatech.content.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/studiomediatech/ContentServiceApplicationTest.java
+++ src/test/java/com/studiomediatech/ContentServiceApplicationTest.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.studiomediatech.content.store.ContentServiceApplication;
+
 
 @RunWith(SpringRunner.class)
 public class ContentServiceApplicationTest {
