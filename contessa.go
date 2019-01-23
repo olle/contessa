67113460d67483777b2ac16af2f296aa4cdb9886
Olle Törnström <olle@studiomediatech.com>
2019-01-23 20:52:00 +0100

    Add startup flags
    * Initially allowing version output `--version` and an optinal
      configuration file path `--config`.

--- contessa.go
+++ contessa.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
+
+	ini "github.com/vaughan0/go-ini"
+)
+
+const version = "0.14.0"
+
+var (
+	config      ini.File
+	configPath  string
+	showVersion bool
 )
 
 func main() {
-	log.Printf("Hello World!")
+	log.Printf("Starting Contessa v%s...", version)
+
+	flag.BoolVar(&showVersion, "version", false, "show current version")
+	flag.StringVar(&configPath, "config", "./config.ini", "configuration file path")
+	flag.Parse()
+
+	if showVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
+	_, err := ini.LoadFile(configPath)
+	if err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/olle/contessa
+
+require github.com/vaughan0/go-ini v0.0.0-20130923145212-a98ad7ee00ec
